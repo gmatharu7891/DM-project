69198b84ec356cceac0b5db0f083522248c0094a
Anastasia Bolotnikova <bolotnikova.anastasia@gmail.com>
2015-12-24 20:46:30 +0200

    Project template, cpp-s added

---
+++ code/path_planning.cpp
@@ -0,0 +1,14 @@
+
+#include "robot.h"
+#include <iostream>
+
+My_Robot_Space::time_t My_Robot_Space::move_a_robot(unsigned gridsize_NS, unsigned gridsize_EW, 
+        Robot_ID_t** robot_in_initial_situation, 
+        const std::list<Robot_Command>& other_robots_commands, Robot_ID_t my_robot, 
+        unsigned x_destination, unsigned y_destination){
+    
+    std::cout << "returns the shortest path" << std::endl;
+    std::cout << "My robot: " << my_robot << std::endl;
+    
+    return 12;
+}
