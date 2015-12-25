0b1f7dca6e05b0670b4c3f26c457232060f6fc0d
Anastasia Bolotnikova <bolotnikova.anastasia@gmail.com>
2015-12-25 11:34:09 +0200

    robot_in_initial_situation type fix

--- code/path_planning.cpp
+++ code/path_planning.cpp
@@ -2,8 +2,9 @@
 #include "robot.h"
 #include <iostream>
 
+// Function to compute the shortest path
 My_Robot_Space::time_t My_Robot_Space::move_a_robot(unsigned gridsize_NS, unsigned gridsize_EW, 
-        Robot_ID_t **robot_in_initial_situation, 
+        Robot_ID_t const *const *robot_in_initial_situation, 
         const std::list<Robot_Command>& other_robots_commands, Robot_ID_t my_robot, 
         unsigned x_destination, unsigned y_destination){
     
@@ -12,3 +13,15 @@ My_Robot_Space::time_t My_Robot_Space::move_a_robot(unsigned gridsize_NS, unsign
     
     return 12;
 }
+
+// Function to generate commands and initial positions for other robots
+
+
+// Function to generate the occupancy lookup table
+
+
+// Function to check if the nodes in the tree are equivalent
+
+
+// Function to generate all possible next moves
+
